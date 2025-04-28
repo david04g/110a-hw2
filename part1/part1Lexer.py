3ce797ec46a65b356b313843d49498223583e03d
david04g <dgarc148@ucsc.edu>
2025-04-28 21:33:29 +0000

    part1 hotfix

--- part1/part1Lexer.py
+++ part1/part1Lexer.py
@@ -1,64 +1,74 @@
 import ply.lex as lex
 
-# Token list
+# Todo: Add all the tokens here
 tokens = [
-    'ID', 'INT', 'FLOAT',
-    'PLUS', 'MINUS', 'MULT', 'DIV',
-    'EQ', 'ASSIGN', 'LT',
-    'LPAREN', 'RPAREN', 'LBRACE', 'RBRACE',
-    'SEMI',
-    'INTTYPE', 'FLOATTYPE',
-    'IF', 'ELSE', 'FOR'
+    'PLUS', 'ID', 'SEMI',
+    'NUM',
+    'MULT',
+    'DIV',
+    'LPAREN',
+    'RPAREN',
+    'LBRACE',
+    'RBRACE',
+    'ASSIGN',
+    'IF',
+    'ELSE',
+    'INT',
+    'FLOAT',
+    'EQ',
+    'LT',
+    'FLOATTYPE',
+    'FOR',
+    'MINUS',
 ]
 
-# Regex rules for simple tokens
-t_PLUS    = r'\+'
-t_MINUS   = r'-'
-t_MULT    = r'\*'
-t_DIV     = r'/'
-t_EQ      = r'=='
-t_ASSIGN  = r'='
-t_LT      = r'<'
-t_LPAREN  = r'\('
-t_RPAREN  = r'\)'
-t_LBRACE  = r'\{'
-t_RBRACE  = r'\}'
-t_SEMI    = r';'
+keywords = {
+        "if": 'IF',
+        "else": 'ELSE',
+        "int": 'INT',
+        "float": 'FLOATTYPE',
+        "for": 'FOR',
+    }
 
-# Reserved words mapping
-reserved = {
-    'int': 'INTTYPE',
-    'float': 'FLOATTYPE',
-    'if': 'IF',
-    'else': 'ELSE',
-    'for': 'FOR'
-}
+# Todo: Add all the requied regular expression rules here
+t_SEMI = r';'
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_MULT = r'\*'
+t_DIV = r'/'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_LBRACE = r'\{'
+t_RBRACE = r'\}'
+t_ASSIGN = r'='
+t_EQ = r'=='
+t_LT = r'<'
 
-# Floating point numbers
-def t_FLOAT(t):
+t_ignore = ' \t\r\n'  # Ignore spaces and tabs
+
+# Todo: Write all the required regular expression rule with action code
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = keywords.get(t.value, 'ID') 
+    return t
+
+def t_FLOAT(t): 
     r'\d+\.\d+'
     t.value = float(t.value)
     return t
 
-# Integers
-def t_INT(t):
+def t_NUM(t):
     r'\d+'
     t.value = int(t.value)
     return t
 
-# Identifiers and keywords
-def t_ID(t):
-    r'[a-zA-Z_][a-zA-Z0-9_]*'
-    t.type = reserved.get(t.value, 'ID')
-    return t
 
-# Ignore whitespace and tabs
-t_ignore = ' \t\r\n'
 
-# Error handling rule
+# Todo: Fix the error function 
 def t_error(t):
-    print(f"Illegal character {t.value[0]}")
+    print(f"Illegal character")
     t.lexer.skip(1)
 
 # Build the lexer
 lexer = lex.lex()
+
--- part1/part1Parser.py
+++ part1/part1Parser.py
@@ -1,7 +1,10 @@
 import argparse
 import ply.yacc as yacc
+
+# Importing tokens map from part1Lexer
 from part1Lexer import lexer, tokens
 
+# Todo: Define all the required grammar and actions
 def p_program(p):
     'program : statement_list'
 
@@ -10,73 +13,55 @@ def p_statement_list(p):
                       | '''
 
 def p_statement(p):
-    '''statement : declaration
-                 | assignment
-                 | if_stmt
-                 | for_stmt
-                 | block'''
+    '''statement : variables
+                  | assignment
+                  | if_statement
+                  | for_statement
+                  | block'''
+    
 
-def p_declaration(p):
-    '''declaration : INTTYPE ID SEMI
-                   | FLOATTYPE ID SEMI'''
+def p_variables(p):
+    '''variables : INT ID SEMI
+                  | FLOATTYPE ID SEMI'''
 
 def p_assignment(p):
     'assignment : ID ASSIGN expression SEMI'
 
-def p_assignment_no_semi(p):
-    'assignment_no_semi : ID ASSIGN expression'
+def p_if_statement(p):
+    'if_statement : IF LPAREN expression RPAREN statement ELSE statement'
 
-def p_if_stmt(p):
-    'if_stmt : IF LPAREN expression RPAREN statement ELSE statement'
-
-def p_for_stmt(p):
-    'for_stmt : FOR LPAREN assignment expression SEMI assignment_no_semi RPAREN statement'
+def p_for_statement(p):
+    'for_statement : FOR LPAREN assignment expression SEMI ID ASSIGN expression RPAREN statement'
 
 def p_block(p):
     'block : LBRACE statement_list RBRACE'
 
-# Expression grammar encoded with precedence by hierarchy
-def p_expression_eq(p):
-    'expression : expression EQ relation'
+def p_expression(p):
+    'expression : equality'
 
-def p_expression_rel(p):
-    'expression : relation'
+def p_equality(p):
+    '''equality : equality EQ less_than
+                | less_than'''
 
-def p_relation_lt(p):
-    'relation : relation LT term'
+def p_less_than(p):
+    '''less_than : less_than LT addition
+                  | addition'''
 
-def p_relation_term(p):
-    'relation : term'
+def p_addition(p):
+    '''addition : addition PLUS multiplication
+                 | addition MINUS multiplication
+                 | multiplication'''
 
-def p_term_add(p):
-    'term : term PLUS factor'
+def p_multiplication(p):
+    '''multiplication : multiplication MULT term
+                      | multiplication DIV term
+                      | term'''
 
-def p_term_sub(p):
-    'term : term MINUS factor'
-
-def p_term_factor(p):
-    'term : factor'
-
-def p_factor_mult(p):
-    'factor : factor MULT unit'
-
-def p_factor_div(p):
-    'factor : factor DIV unit'
-
-def p_factor_unit(p):
-    'factor : unit'
-
-def p_unit_group(p):
-    'unit : LPAREN expression RPAREN'
-
-def p_unit_id(p):
-    'unit : ID'
-
-def p_unit_int(p):
-    'unit : INT'
-
-def p_unit_float(p):
-    'unit : FLOAT'
+def p_term(p):
+    '''term : LPAREN expression RPAREN
+            | ID
+            | NUM
+            | FLOAT'''
 
 def p_error(p):
     if p:
@@ -84,25 +69,36 @@ def p_error(p):
     else:
         print("Syntax error at EOF")
 
-parser = yacc.yacc()
+def test_parser(input_string):
+    result = parser.parse(input_string, lexer=lexer)
+    if result is None:
+        print("Input matches the grammar.")
+    else:
+        print("Input does not match the grammar.")
+
+# Build the parser
+ply_parser = yacc.yacc(debug=True)
 
 if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument('file_name', type=str, help="Input file containing the text to parse.")
     args = arg_parser.parse_args()
-
+    
     try:
         with open(args.file_name, 'r') as file:
             f_contents = file.read()
+            #print("File contents read successfully:")
+            #print(f_contents)
     except FileNotFoundError:
         print(f"Error: File '{args.file_name}' not found.")
         exit()
 
     try:
-        result = parser.parse(f_contents, lexer=lexer)
+        result = ply_parser.parse(f_contents, lexer=lexer)
         if result is None:
             print("Input matches the grammar.")
         else:
             print("Input does not match the grammar.")
     except Exception as e:
         print(f"Error during parsing: {e}")
+
